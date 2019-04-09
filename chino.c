92e68ba649209ddedb4ee248f1ceb5cf9850a649
Leon Reynosa <37784693+Leon7reynosa@users.noreply.github.com>
2019-04-09 19:32:03 -0300

    dada

--- chino.c
+++ chino.c
@@ -1,64 +1,45 @@
 /*
- * Lissandra.h
+ * Lissandra.c
  *
- *  Created on: 9 abr. 2019
+ *  Created on: 8 abr. 2019
  *      Author: utnso
  */
 
-#ifndef LISSANDRA_H_
-#define LISSANDRA_H_
+#include "Lissandra.h"
 
-	#include<stdio.h>
-	#include<stdlib.h>
-	#include<commons/log.h>
-	#include<commons/string.h>
-	#include<commons/config.h>
-	#include<readline/readline.h>
-	
-	t_logger* LissandraLogger;
+int main(void){
 
-	//Por ahora pongo aca
+	iniciar_log();
 
-	/*
-	 * typedef struct{
-	 * 		key (Tiene q ser uint 16)
-	 * 		value
-	 * 		timestamp
-	 *
-	 * 		} dato
-	 *
-	 */
-
-
-	//aca tenemos la metadata en la cual se va a almacenar los "dato" para luego hacerlos temporales.
-	// dato memtable[ 10 ] ;
+	leer_consola();
 
 
 
-		//obtiene el valor de una key dentro de una tabla
-
-		//crea una nueva tabla dentro del file system
-		void create( /* char *nombre_tabla , int *key */);
-			//Por cada tabla creada, hay que hacer un metadata propio de ese (Esta en un issue)
+	return EXIT_SUCCESS;
+}
 
 
-		//ingresa o actualiza una key dentro de una tabla
-		void insert( void /* char *nombre_tabla , int timestamp , int key , char *valor */);
-			/*
-			 * Ejm. por consola pasas
-			 *
-			 * insert(TablaEjm, 1000, 1, "Martin")
-			 *
-			 * 		se deberia llamar a la funcion "insertarRegistro(con los mismos parametros)" y crear el registro del tipo "dato" (definido en el struct)
-			 * 		insertarRegistro() va a meter el "dato" en el array "memtable", luego de cierto tiempo se crea el archivo temporal con los datos de la memtable y se vacia.
-			 */
+void iniciar_log(void){
 
-		//permite obtener la metadata de una tabla
-		void describe( void /* char *nombre_tabla */ );
+	char* Lisandra_ruta = "/home/utnso/Escritorio/TP_Sistemas_Operativos/tp-2019-1c-Te-Lo-Testeo-Asi-Nom-s/Lissandra File System/FLS.log";
 
-		//elimina una tabla
-		void drop( void /* char *nombre_tabla */);
+	logger_Lisandra = log_create( Lisandra_ruta , "Lissandra" , 1 , LOG_LEVEL_INFO );
+}
+
+void leer_consola(void)
+{
+	char* leido;
+
+	leido = readline(">");
 
 
+	while(*leido != '\0') {
 
-#endif /* LISSANDRA_H_ */
+		log_info(g_logger, leido);
+		free(leido);
+		leido = readline(">");
+	}
+
+	free (leido);
+
+}
